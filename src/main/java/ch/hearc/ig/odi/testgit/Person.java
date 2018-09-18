a82f1abec6b66eaf0c7123a301294d9e224632a2
dylan-sk8 <dylan-sk8@hotmail.com>
2018-09-18 09:25:20 +0200

    implementation of the person class

---
+++ src/main/java/ch/hearc/ig/odi/testgit/Person.java
@@ -0,0 +1,42 @@
+package ch.hearc.ig.odi.testgit;
+
+import java.util.Date;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+
+    public Person(String firstName, String lastName, Date birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
